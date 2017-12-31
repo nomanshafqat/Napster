332cf2b01513ca60c869381da6a79a3cab5fe1e0
Noman Shafqat <14besemshafqat@seecs.edu.pk>
2018-01-01 02:58:35 +0500

    search done

--- Client/Client.py
+++ Client/Client.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import shutil
@@ -7,7 +6,7 @@ import socketserver
 import threading
 import time
 from threading import Thread
-from Utils.Send_Data import send_data,send_data_sock,send_binary_file
+from Utils.Send_Data import send_data, send_data_sock, send_binary_file
 from Utils.Send_Data import send_data
 from Utils.Receive_Data import receive_data
 
@@ -15,29 +14,28 @@ from Utils.Receive_Data import receive_data
 class ThreadedTCPClientRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
 
-        #receive the length
+        # receive the length
         self.data = self.request.recv(32).strip()
-        length=int(str(self.data.decode('utf-8')))
-        print("incoming packet length",length)
+        length = int(str(self.data.decode('utf-8')))
+        print("incoming packet length", length)
 
-
-        #read the data
+        # read the data
         self.data = self.request.recv(length).strip()
         jsondata = json.loads(str(self.data.decode('utf-8')))
 
-        if jsondata["command"]=="get":
+        if jsondata["command"] == "get":
             print("client receive the file request")
-            fileid=jsondata["fileid"]
+            fileid = jsondata["fileid"]
             global dic
             print(dic)
-            path=os.path.join(dic[fileid][1],dic[fileid][0])
+            path = os.path.join(dic[fileid][1], dic[fileid][0])
             f = open(path, 'rb')
-            filecontents=f.read()
-            send_binary_file(filecontents,"sds",self.request)
+            filecontents = f.read()
+            send_binary_file(filecontents, "sds", self.request)
             print("File Sent")
 
-        if jsondata["command"]=="ping":
-            #TODO ACK to ping
+        if jsondata["command"] == "ping":
+            # TODO ACK to ping
             return
 
 
@@ -45,37 +43,35 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 
+# dic["file_id"] = [filename,path]
+dic = {}
+hostingat = ("localhost", 9435)  # client hosting at
 
-#dic["file_id"] = [filename,path]
-dic={}
-hostingat=("localhost", 9435) #client hosting at
 
 def get_server_address_port():
     return "localhost", 9999
 
 
-
 def get_unique_id_from_server(filename):
-
     global hostingat
-    data = {"command": "seed", "filename": filename,"ip":hostingat[0],"port":hostingat[1]}
+    data = {"command": "seed", "filename": filename, "ip": hostingat[0], "port": hostingat[1]}
     data = json.dumps(data)
     HOST, PORT = get_server_address_port()
 
-
-    sock=send_data(data,HOST,PORT)
-    id=receive_data(sock).decode('utf-8')
+    sock = send_data(data, HOST, PORT)
+    id = receive_data(sock).decode('utf-8')
 
     return id
 
 
-def seedfile(filename,path):
+def seedfile(filename, path):
     global dic
-    print("Sending Seeding request for " +filename+" in " +path)
-    id=get_unique_id_from_server(filename)
-    dic[id]=[filename,path]
+    print("Sending Seeding request for " + filename + " in " + path)
+    id = get_unique_id_from_server(filename)
+    dic[id] = [filename, path]
     print(dic)
     return
+
 
 def convert_to_json(data):
     return json.loads(str(data.decode('utf-8')))
@@ -86,15 +82,14 @@ def getfile(file_id):
     data = json.dumps(data)
     HOST, PORT = get_server_address_port()
 
-
     sock = send_data(data, HOST, PORT)
     clientinfo = receive_data(sock).decode('utf-8')
 
     jsondata = json.loads(clientinfo)
 
-    sock=send_data(data, jsondata["ip"], int(jsondata["port"]))
-    data=receive_data(sock)
-    f=open(jsondata["filename"],"wb")
+    sock = send_data(data, jsondata["ip"], int(jsondata["port"]))
+    data = receive_data(sock)
+    f = open(jsondata["filename"], "wb")
     f.write(data)
     f.close()
     print("file received")
@@ -105,9 +100,14 @@ def search(q):
     data = json.dumps(data)
     HOST, PORT = get_server_address_port()
 
-
     sock = send_data(data, HOST, PORT)
     clientinfo = receive_data(sock).decode('utf-8')
+    clientinfo=clientinfo.split(",")
+    print(clientinfo)
+    for i in range(0,len(clientinfo),2):
+
+        print(clientinfo[i][3:-1],clientinfo[i+1][2:-2])
+
 
 
     return
@@ -138,15 +138,23 @@ if __name__ == "__main__":
     search [string]
 get sdfsfsfdsf
 seed miley_cyrus_wrecking_ball_My2FRPA3Gf8_320kbps.mp3 /Users/nomanshafqat/Downloads
+search miley_cyrus_wrecking_ball_My2FRPA3Gf8_320kbps
     '''
     while (True):
-        command = input('>>')
-        arg=command.split(" ")
+        try:
 
-        if arg[0]=="seed":
-            seedfile(arg[1],arg[2])
-        if arg[0]=="get":
-            getfile(arg[1])
-        if arg[0]=="search":
-            search(arg[1])
+            command = input('>>')
+            arg = command.split(" ")
 
+            if arg[0] == "seed":
+                seedfile(arg[1], arg[2])
+            if arg[0] == "get":
+                getfile(arg[1])
+            if arg[0] == "search":
+                search(arg[1])
+        except:
+            print("Bad Param")
+            print('''seed  [filename] [path]
+            get [file_id]
+            search [string]''')
+            continue
--- Server/Server.py
+++ Server/Server.py
@@ -2,60 +2,76 @@ import json
 import os
 import socketserver
 import threading
-from Utils.Send_Data import send_data,send_data_sock
+from Utils.Send_Data import send_data, send_data_sock
 from Utils.Receive_Data import receive_data
 import string
 import random
+from difflib import SequenceMatcher
 
 dic = {}
 ''' "id" : ["filename","ip","port"]'''
 
 
-
 def generate_unique_file_id():
-    randomnum = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(0,64)])
+    randomnum = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(0, 64)])
     return randomnum
 
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         global dic
-        #receive the length
+        # receive the length
         self.data = self.request.recv(32).strip()
-        length=int(str(self.data.decode('utf-8')))
-        print("incoming packet length",length)
+        length = int(str(self.data.decode('utf-8')))
+        print("incoming packet length", length)
 
-        #read the json  data
+        # read the json  data
         self.data = self.request.recv(length).strip()
         jsondata = json.loads(str(self.data.decode('utf-8')))
 
-
-        if jsondata["command"]=="seed":
-            print("Seed request",self.client_address)
-            id=generate_unique_file_id()
-            send_data_sock(id,self.request)
+        if jsondata["command"] == "seed":
+            print("Seed request", self.client_address)
+            id = generate_unique_file_id()
+            send_data_sock(id, self.request)
             print("Id sent...")
-            dic[id]=[jsondata["filename"],jsondata["ip"],jsondata["port"]]
+            dic[id] = [jsondata["filename"], jsondata["ip"], jsondata["port"]]
             print(dic)
-        elif jsondata["command"]=="get":
-            print("get request",self.client_address)
+        elif jsondata["command"] == "get":
+            print("get request", self.client_address)
             print(jsondata)
-            filename_ip_port=dic[jsondata["fileid"]]
+            filename_ip_port = dic[jsondata["fileid"]]
 
-            data = {"command": "getserreply","filename":filename_ip_port[0], "ip": filename_ip_port[1],"port":filename_ip_port[2]}
+            data = {"command": "getserreply", "filename": filename_ip_port[0], "ip": filename_ip_port[1],
+                    "port": filename_ip_port[2]}
             data = json.dumps(data)
 
             print(str(filename_ip_port))
-            send_data_sock(data,self.request)
+            send_data_sock(data, self.request)
 
-        elif jsondata["command"]=="search":
-            print("search request",self.client_address)
+        elif jsondata["command"] == "search":
+            print("search request", self.client_address)
+            q = jsondata["q"]
+
+            searchresults = [("key","filename")]
+
+            if len(dic.keys()) > 0:
+                for key in dic.keys():
+                    if similar(dic[key][0], q) > 0.9:
+                        searchresults.append(( key,dic[key][0]))
+                    elif q in dic[key][0]:
+                        searchresults.append(( key,dic[key][0]))
+
+            searchresults = str(searchresults)
+
+            send_data_sock(searchresults, self.request)
 
 
             return
             # TODO search in the dic for filename and return the filename and file id from dic[file_id]\
-
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -63,13 +79,13 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 def check_clients():
-    #TODO make a thread in main to ping every client after 10mins
+    # TODO make a thread in main to ping every client after 10mins
     global dic
-    if len(dic.keys())>0:
+    if len(dic.keys()) > 0:
         print(dic)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
 
     servername = "server"
     if not os.path.exists(servername):
